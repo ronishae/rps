7f480d51274d1b0c472fe4d6ddd03a56ff45879d
Roni <roni@example.com>
2025-10-18 15:01:08 -0400

    config

---
+++ config.js
@@ -0,0 +1,16 @@
+// IMPORTANT: Replace the placeholder values below with your actual Firebase credentials.
+
+// 1. The custom App ID used for structuring Firestore documents
+export const APP_ID = 'basic-rps-online';
+
+// 2. The configuration object provided by the Firebase Console when you set up your web app
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDVLnIsP9WxkIqY8rUoP0ipAnFZRmymN54",
+    authDomain: "basic-rps-online.firebaseapp.com",
+    projectId: "basic-rps-online",
+    storageBucket: "basic-rps-online.firebasestorage.app",
+    messagingSenderId: "541709698963",
+    appId: "1:541709698963:web:2882a77aa04b1419970a66"
+};
+
+export const RECAPTCHA_SITE_KEY = "6Lfv8O4rAAAAAEcrtLifIpvDeQp3VTOF2Iy0GX0I";
